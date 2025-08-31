20a11bd8c899eb3a0df64fee401a9ea3f5122556
Himanshu Beniwal <beniwalhimanshu288@gmail.com>
2025-08-31 20:32:38 +0530

    Remove data folder from .gitignore - contains essential locations list

---
+++ frontend/src/data/locations.js
@@ -0,0 +1,19 @@
+export const locationsList = [
+  "India",
+  "Andaman and Nicobar Islands, India",
+  "Assam,India",
+  "Andhra Pradesh,India",
+  "Bihar,India", 
+  "Delhi,India",
+  "Gujarat,India",
+  "Haryana,India",
+  "Jammu and Kashmir,India",
+  "Karnataka,India",
+  "Kerala,India",
+  "Maharashtra,India",
+  "Meghalaya,India",
+  "Madhya Pradesh,India",
+  "Odisha,India",
+  "Punjab,India",
+  // Your full list
+];
